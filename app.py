604ec4e91cc6d558a71a412dcbd4391ef7b603a4
DajungJe <ss01363@webtoonscorp.com>
2018-12-06 16:30:57 +0900

    Update template and show mem

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import subprocess
+from functools import partial
 
 from flask import Flask, render_template
 
 def execute(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT):
     process = subprocess.Popen(commands, shell=shell, stdout=stdout, stderr=stderr)
     out, err = process.communicate()
-    return (out if not err else err).decode('utf-8').split()
+    return (out if not err else err).decode('utf-8')
 
 def inspect():
-    containers = execute('docker ps --format "{{ .Names }},{{ .Status }}"')
-    processes = list(map(lambda c: list(map(int, execute("docker top {} | awk '{}'".format(c, '{p=1} p && $2 ~ /^[0-9]+$/ {print $2}')))), containers))
-    devices = list(map(int, execute('''nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2, $3}' ''')))
+    containers = list(map(partial(str.split, sep=','), execute('docker ps --format "{{ .Names }},{{ .Status }}"').splitlines()))
+    processes = list(map(lambda c: execute("docker top {} | awk '{}'".format(c[0], '{p=1} p && $2 ~ /^[0-9]+$/ {print $2}')).split(), containers))
+    devices = execute('''nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2, $3, $6}' ''').split()
 
-    for gpu, proc in zip(devices[::2], devices[1::2]):
-        yield gpu, proc, *next(c.split(',') for c, ps in zip(containers, processes) if proc in ps)
+    for gpu, proc, mem in zip(devices[::3], devices[1::3], devices[2::3]):
+        yield (gpu, proc, mem, *next(c for c, ps in zip(containers, processes) if proc in ps))
 
 app = Flask(__name__)
 

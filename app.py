cc822aa99185df03b34ec66aa853ab2c0b708207
DajungJe <ss01363@webtoonscorp.com>
2018-12-06 16:05:31 +0900

    Update scripts

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
 import subprocess
 
+from flask import Flask, render_template
+
 def execute(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT):
     process = subprocess.Popen(commands, shell=shell, stdout=stdout, stderr=stderr)
     out, err = process.communicate()
-    return (out if not err else err).split()
+    return (out if not err else err).decode('utf-8').split()
 
 def inspect():
-    devices = execute('''nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2, $3}' ''')
+    devices = list(map(int, execute('''nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2, $3}' ''')))
     containers = execute('docker ps --format "{{ .Names }}"')
-    processes = map(lambda c: list(map(int, execute("docker top {} | awk '{}'".format(c, '{p=1} p && $2 ~ /^[0-9]+$/ {print $2}')))), containers)
-    gpus = devices[::2]
-    gpuprocs = map(int, devices[1::2])
-    for gpu, proc in zip(gpus, gpuprocs):
-        belongs = []
-        for container, containerproc in zip(containers, processes):
-            if proc in containerproc:
-                belongs = container
-
-
-listOdd = list1[1::2] # Elements from list1 starting from 1 iterating by 2
-listEven = list1[::2]
-
-awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2","$3}'
-
-p = subprocess.Popen('ls', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-for line in p.stdout.readlines():
-    print line,
-retval = p.wait()
-
-@app.route('/')
-def hello():
-    return "Hello World!"
+    processes = list(map(lambda c: list(map(int, execute("docker top {} | awk '{}'".format(c, '{p=1} p && $2 ~ /^[0-9]+$/ {print $2}')))), containers))
+    gpus, gprocs = devices[::2], devices[1::2]
+    
+    for gpu, proc in zip(gpus, gprocs):
+        belongs = next(c for c, ps in zip(containers, processes) if proc in ps)
+        yield gpu, proc, belongs
 
 app = Flask(__name__)
 
+@app.route('/')
+def blame():
+    gpus = list(inspect())
+    print (gpus)
+    return render_template('blame.html', gpus=gpus)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=6006, debug=True)
+

4d0314d1cfec511462e396079faa533412520552
DajungJe <ss01363@webtoonscorp.com>
2018-12-10 14:01:39 +0900

    Update fix cpu memory

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ class Inspect:
     def __inspect():
         containers = list(map(partial(str.split, sep=','), Inspect.__exe('docker ps --format "{{ .Names }},{{ .Status }}"').splitlines()))
         processes = list(map(lambda c: Inspect.__exe("docker top {} | awk '{}'".format(c[0], '{p=1} p && $2 ~ /^[0-9]+$/ {print $2}')).split(), containers))
-        stats = list(map(str.split, Inspect.__exe("docker stats --no-stream | awk 'NR-1 > 0 { print $3, $4 }'").splitlines()))
+        stats = {line.split()[0]: line.split()[1:] for line in Inspect.__exe("docker stats --no-stream | awk 'NR-1 > 0 { print $2, $3, $4 }'").splitlines()}
         devices = Inspect.__exe('''nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2, $3}' ''').split()
         utils = list(map(partial(str.split, sep=','), Inspect.__exe('''nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv,noheader''').splitlines()))
-
+        
         for gpu, proc in zip(map(int, devices[::2]), devices[1::2]):
-            yield (gpu, [proc, *next(c for c, ps in zip(containers, processes) if proc in ps), *stats[gpu], *utils[gpu]])
+            container, status = next(c for c, ps in zip(containers, processes) if proc in ps)
+            yield (gpu, [proc, container, status, *stats[container], *utils[gpu]])
 
     @staticmethod
     def update():

d0dc6f10b68f2410da11eef61816de014879f300
jiun.bae <jiun.bae@webtoonscorp.com>
2018-12-10 13:50:23 +0900

    Update to inspect cpu and local memory

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class Inspect:
     cache = list()
     last = datetime.now()
 
-    columns = "#,PID,Container,Status,CPU,Memory".split(',')
+    columns = "#,PID,Container,Status,CPU,Memory,GPU,Memory".split(',')
 
     @staticmethod
     def __exe(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT):
@@ -21,11 +21,12 @@ class Inspect:
     def __inspect():
         containers = list(map(partial(str.split, sep=','), Inspect.__exe('docker ps --format "{{ .Names }},{{ .Status }}"').splitlines()))
         processes = list(map(lambda c: Inspect.__exe("docker top {} | awk '{}'".format(c[0], '{p=1} p && $2 ~ /^[0-9]+$/ {print $2}')).split(), containers))
+        stats = list(map(str.split, Inspect.__exe("docker stats --no-stream | awk 'NR-1 > 0 { print $3, $4 }'").splitlines()))
         devices = Inspect.__exe('''nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 ~ /^[0-9]+$/ {print $2, $3}' ''').split()
         utils = list(map(partial(str.split, sep=','), Inspect.__exe('''nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv,noheader''').splitlines()))
 
-        for gpu, proc in zip(devices[::2], devices[1::2]):
-            yield (gpu, [proc, *next(c for c, ps in zip(containers, processes) if proc in ps), *utils[int(gpu)]])
+        for gpu, proc in zip(map(int, devices[::2]), devices[1::2]):
+            yield (gpu, [proc, *next(c for c, ps in zip(containers, processes) if proc in ps), *stats[gpu], *utils[gpu]])
 
     @staticmethod
     def update():
